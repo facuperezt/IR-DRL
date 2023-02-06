905f61083d370902e8ef5ac40fce75b8bf9e6312
FaQ <facuperezt@googlemail.com>
2023-02-06 18:04:42 +0100

    training ready framestacking

--- custom_policies/dropout_policy.py
+++ custom_policies/dropout_policy.py
@@ -62,7 +62,8 @@ class CustomizableCNN(BaseFeaturesExtractor):
             observations = observations.reshape(-1, *observations.shape[2:])
             out :th.Tensor = self.linear(self.cnn(observations)).reshape(observations.shape[0]//self.n_frames, self.n_frames, -1)
 
-            print(out.device, self.future_biased_weighting.device)
+            if out.device != self.future_biased_weighting.device:
+                self.future_biased_weighting.to(out.device)    
 
             out = (self.future_biased_weighting * out).mean(dim=1)
         
