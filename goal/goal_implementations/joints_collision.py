d950717f254ba7d65633b75edc94f3b867a93d3f
FaQ <facuperezt@googlemail.com>
2023-02-27 20:46:12 +0100

    train 100 spheres

--- goal/goal_implementations/joints_collision.py
+++ goal/goal_implementations/joints_collision.py
@@ -172,12 +172,11 @@ class JointsCollisionGoal(Goal):
                 self.done = True
                 self.is_success = True
             reward += self._steps_within_range * self.reward_success / 5
-        elif step > self.max_steps:
+        elif step > self.max_steps or self._steps_within_range > 0:
             self.done = True
             self.timeout = True
             reward += self.reward_collision / 2
         else:
-            self._steps_within_range = 0
             self.done = False
             reward += self.reward_distance_mult * self.distance
         
